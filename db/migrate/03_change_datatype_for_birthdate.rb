58c2df4a6be1e23822cb3c3726319ad0f64e5839
Riley Moore <rilemoore@gmail.com>
2019-11-13 00:11:52 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -2,7 +2,7 @@ class ChangeDataTypeForBirthdate
   
   def change(table_name, column_name, type)
     
-    change_column(table_name, column_name, type)
+    change_column(, column_name, type)
     
     
   end
