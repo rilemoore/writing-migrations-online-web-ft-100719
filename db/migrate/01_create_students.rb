0b1e7101a92804d73bf0f9c1a25f3cc8f0afbdbe
Riley Moore <rilemoore@gmail.com>
2019-11-13 00:15:31 +0000

    Done.

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,12 @@
+#Create the table with its attributes
+
+
 class CreateStudents < ActiveRecord::Migration[5.1]
+  
+  def change
+    create_table :students do |t|
+      t.string :name
+    end
+  end
+  
 end
