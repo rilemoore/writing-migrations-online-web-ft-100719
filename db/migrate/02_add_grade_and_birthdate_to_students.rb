a6b373c3e2cbf27fbb8997b1f7470fdce15c64e2
Riley Moore <rilemoore@gmail.com>
2019-11-13 00:01:07 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,4 +1,4 @@
-def class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
+class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
 
   def change
     add_column :grade, :integer
